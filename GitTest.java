c48feb32a3d5226f9327451efaf7ac3406b1ecd1
gsakul21 <gsakul21@yahoo.com>
2018-12-13 12:16:18 -0500

    Update GitTest.java

--- GitTest.java
+++ GitTest.java
@@ -3,8 +3,10 @@ public class GitTest{
 private String text;
 
 public GitTest(){
-this.text = "\"This is my GitHub test\".";
+this.text = "This is my GitHub test";
 }
 
-
+public String toString(){
+	return "\""+text+"\".";
+	}
 }
